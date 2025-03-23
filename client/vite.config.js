e55a57daa79dd29f4f47ba8b9a8528a5ab2e9b1e
User <user@example.com>
2025-03-24 03:16:05 +0530

    Updated routing to use HashRouter for GitHub Pages and updated for custom domain setup

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ELB/',
+  base: '/',
   server: {
     proxy: {
       '/api': {
@@ -17,4 +17,4 @@ export default defineConfig({
       overlay: false
     }
   }
-})
+}) 
